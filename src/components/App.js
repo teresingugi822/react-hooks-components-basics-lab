a1b9b2df28123d050bc4ede396430772593a7e16
teresingugi822 <teresianyathira2007@gmail.com>
2025-04-12 14:51:10 +0300

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,14 @@
-import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
-
+import React from 'react';
+import Navbar from './Navbar'; 
+import Home from './Home';     
+import About from './About';  
 
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
